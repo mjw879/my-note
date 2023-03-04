eaecad73d6bcb0b0a5d1a5f00637c3a4acd8e8c8
majianwei <879913308@qq.com>
2023-03-04 19:25:18 +0800

    新增算法目录，更改目录编排，添加c++的demo

--- Programing Language/Python/LeetCode.py
+++ Programing Language/Python/LeetCode.py
@@ -1,17 +1,26 @@
 from util.LeetCodeUtils import *
 
+class Solution:
+    def countTriplets(self, nums: List[int]) -> int:
+        c=Counter([i&j for i in nums for j in nums])
+
+        res=0
+        for i in nums:
+            t=~i&0xfffffff
+            res+=c[i]
+        return res
 
 
 s=Solution()
-func=s.
+func=s.countTriplets
 paramNumber=str(inspect.signature(func)).count(',')+1
 null=None
 for case,answer in zip(
 [
-   
+   [2,1,3]
 ],
 [
-   
+   12
 ]
 ):    
     if paramNumber==1:
